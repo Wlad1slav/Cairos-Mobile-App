0ab00dcd2e5d3799ace2fba0752ab23f640daab9
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-01 09:45:59 +0300

    Added setNewRows method
    Improved comments

--- hooks/useStorage.ts
+++ hooks/useStorage.ts
@@ -1,17 +1,23 @@
-import { Storage } from "@ionic/storage";
-import { useEffect, useState } from "react";
+import {Storage} from "@ionic/storage";
+import {useEffect, useState} from "react";
 
 const STORAGE_NAME = 'local-storage'
 
 export interface Item<T> {
     id: string;
-    values: T; // Uses the passed interface
+    values: T;
 }
 
+/**
+ * Custom hook for managing local storage with Ionic Storage.
+ *
+ * @param storageKey - The key used to store and retrieve data in local storage.
+ */
 export function useStorage<T>(storageKey: string) {
     const [store, setStore] = useState<Storage>()
     const [rows, setRows] = useState<Item<T>[]>([])
 
+    // Initialize the storage on component mount
     useEffect(() => {
         const initStorage = async () => {
             const newStore = new Storage({
@@ -20,33 +26,41 @@ export function useStorage<T>(storageKey: string) {
             const store = await newStore.create();
             setStore(store);
 
+            // Retrieve stored data or initialize with an empty array
             const storedTools = await store.get(storageKey) || [];
             setRows(storedTools);
         }
         initStorage();
     }, []);
 
-    /* Create new row */
+    /**
+     * Adds a new row to the local storage.
+     *
+     * @param value - The value to be added to the storage.
+     */
     const addRow = async (value: T) => {
-        const newTodo = {
+        const newRow = {
             id: new Date().getTime().toString(),
             values: value
         }
-        const updatedTodos = [...rows, newTodo];
-        setRows(updatedTodos);
-        console.log(updatedTodos);
-        store?.set(storageKey, updatedTodos);
+        const updatedRows = [...rows, newRow];
+        setRows(updatedRows);
+        store?.set(storageKey, updatedRows);
     }
 
-    /* Update existing row */
+    /**
+     * Updates an existing row in the local storage.
+     *
+     * @param id - The ID of the row to update.
+     * @param newValue - The new values to update in the row.
+     */
     const updateRow = async (id: string, newValue: Partial<T>) => {
         const toUpdate = [...rows];
 
-        // Finds a row that changes in local storage
-        let row = rows.filter(todo => todo.id === id)[0];
+        // Find the row that needs to be updated
+        let row = rows.filter(row => row.id === id)[0];
 
-        // The specific rows of the object that were passed are updated.
-        // Others do not change.
+        // Update the specific properties of the row's values
         row.values = { ...row.values, ...newValue };
 
         setRows(toUpdate);
@@ -54,5 +68,29 @@ export function useStorage<T>(storageKey: string) {
         return store?.set(storageKey, rows);
     }
 
-    return { rows: rows, addRow: addRow, updateRow: updateRow }
+    /**
+     * Sets new rows in the local storage, replacing any existing data.
+     *
+     * @param values - An array of values to be stored.
+     */
+    const setNewRows = async (values: Array<T>) => {
+
+        const newRows: Item<T>[] = values.map((value) => {
+            return {
+                id: new Date().getTime().toString(),
+                values: value
+            }
+        });
+
+        setRows(newRows);
+
+        return store?.set(storageKey, newRows);
+    }
+
+    return {
+        rows: rows,
+        addRow: addRow,
+        updateRow: updateRow,
+        setNewRows: setNewRows
+    };
 }
