9966a37dbfd3f6b05f4ea7a57e4ec36961dff7df
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-02 17:55:24 +0300

    Removed hard code

--- src/components/AppAuthOffer.tsx
+++ src/components/AppAuthOffer.tsx
@@ -3,6 +3,7 @@ import {IonButton, IonIcon, IonTitle} from "@ionic/react";
 import {logIn} from "ionicons/icons";
 
 import './AppAuthOffer.scss';
+import routes from "../config/routes.config";
 
 const AppAuthOffer: React.FC = () => {
     return (
@@ -10,8 +11,8 @@ const AppAuthOffer: React.FC = () => {
             <IonIcon icon={logIn} size={'large'} />
             <IonTitle color={'primary'}>Ви ще не авторизувалися</IonTitle>
             <div className="buttons">
-                <IonButton color={'secondary'} href={'/register'}>Реєстрація</IonButton>
-                <IonButton color={'secondary'} fill={'outline'} href={'/auth'}>Увійти</IonButton>
+                <IonButton color={'secondary'} href={routes.registration.url}>Реєстрація</IonButton>
+                <IonButton color={'secondary'} fill={'outline'} href={routes.authorization.url}>Увійти</IonButton>
             </div>
         </div>
     );
