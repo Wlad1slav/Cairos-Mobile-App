01c5635dead06846f1d05daec5b5338d9ff3703c
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-09 20:58:39 +0300

    Created the logic of counting days before the end of the year

--- src/components/AppTimeBeforeYearEnd.tsx
+++ src/components/AppTimeBeforeYearEnd.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {IonIcon} from "@ionic/react";
 import {accessibility, alarm, alert, apps, bag, balloon, ban, cloudyNight} from "ionicons/icons";
 
 import './AppTimeBefore.scss';
 
 const AppTimeBeforeYearEnd: React.FC = () => {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const endOfYear = new Date(currentYear, 11, 31); // December 31 of the current year
+    const millisecondsPerDay = 1000 * 60 * 60 * 24;
+
+    const [daysRemaining, setDaysRemaining] = useState(0);
+
+    useEffect(() => {
+        const remaining = Math.ceil((endOfYear.getTime() - today.getTime()) / millisecondsPerDay);
+        setDaysRemaining(remaining);
+    }, [endOfYear, millisecondsPerDay, today]);
+
     return (
         <div className='hour-before-sleep'>
             <IonIcon icon={balloon} />
-            <p>200 днів до кінця року</p>
+            <p>{daysRemaining} днів до кінця року</p>
         </div>
     );
 }
