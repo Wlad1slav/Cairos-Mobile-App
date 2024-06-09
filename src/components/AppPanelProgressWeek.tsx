1ae9e0b582d205c0c3b20eb66347611e51c4db5a
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-09 20:30:09 +0300

    Created logic for calculating the current day of the week

--- src/components/AppPanelProgressWeek.tsx
+++ src/components/AppPanelProgressWeek.tsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { IonIcon } from "@ionic/react";
+import { arrowForward } from "ionicons/icons";
 
 import './AppPanelProgressWeek.scss';
-import {IonIcon} from "@ionic/react";
-import {arrowForward} from "ionicons/icons";
 
 const AppPanelProgressWeek: React.FC = () => {
+    const lastDayOfWeek = 7;
+    const todayDate = new Date();
+    const [dayNumber, setDay] = useState(todayDate.getDay() || lastDayOfWeek);
+
+    useEffect(() => {
+        if (dayNumber === 0) setDay(lastDayOfWeek);
+    }, [dayNumber]);
+
+    const daysOfWeek = [1, 2, 3, 4, 5, 6, 7];
+
     return (
-        <div className='panel--progress-week'>
+        <div className="panel--progress-week">
             <div className="progress">
-                <p>•</p>
-                <p>•</p>
-                <p>•</p>
-                <p>•</p>
-                <p className='left today'>•</p>
-                <p className='left'>•</p>
-                <p className='left'>•</p>
+                {daysOfWeek.map((number) => (
+                    <p
+                        key={number}
+                        className={`${dayNumber === number ? 'today' : ''} ${dayNumber <= number ? 'left' : ''}`}
+                    >
+                        •
+                    </p>
+                ))}
             </div>
             <div className="dayer">
                 <b>П'ятниця</b>
                 <IonIcon icon={arrowForward} />
-                <p>2 дня до кінця тижня</p>
+                <p>
+                    {lastDayOfWeek + 1 - dayNumber} {lastDayOfWeek + 1 - dayNumber === 1 ? 'день' : 'дня'} до кінця тижня
+                </p>
             </div>
         </div>
     );
-}
+};
 
 export default AppPanelProgressWeek;
