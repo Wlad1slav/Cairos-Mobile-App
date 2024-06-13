ed6cb0783bce89f3eed79b27ae340c3beac421e8
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-13 12:05:35 +0300

    Created the logic of the Cairos accent

--- src/components/AppCairosAccent.tsx
+++ src/components/AppCairosAccent.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
+import cairos from "../config/cairos-philosophy.config";
+import getWeekDay from "../utils/datetime.getWeekDay";
+
 import './AppCairosAccent.scss';
 
 const AppCairosAccent: React.FC = () => {
+
+    const dayNumber = getWeekDay();
+
     return (
         <div className='cariros-accent'>
-            <h1>
-                <span>Акцент</span> Логіка
-            </h1>
+            <div className="short-info">
+                <span>
+                    ( {cairos[dayNumber].symbol} )
+                </span>
+                <h1>
+                    {cairos[dayNumber].name}
+                </h1>
+            </div>
+
+            <p className="description">
+                {cairos[dayNumber].description}
+            </p>
         </div>
     );
 }
